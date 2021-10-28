a1b06fe3a7f21775c9ae29bf66dc6c92e5ee6b6b
Kevin Tse <ktse@fb.com>
2021-10-28 17:48:53 -0400

    Update on "[hackathon][DataPipe] adding .pyi file generation for torch.utils.data.datapipes"
    This is a work in progress.
    
    Related TorchData issue: https://github.com/pytorch/data/issues/80
    
    cc @VitalyFedyunin ejguan @NivekT
    
    [ghstack-poisoned]

--- torch/utils/data/gen_pyi.py
+++ torch/utils/data/gen_pyi.py
@@ -1,5 +1,6 @@
 import os
 from typing import List
+from tools.codegen.gen import FileManager
 
 """
 .pyi generation for functional DataPipes Process
@@ -14,6 +15,7 @@ from typing import List
 files_to_exclude = {"__init__.py", "utils.py"}
 deprecated_files = {"httpreader.py", "linereader.py", "tararchivereader.py", "ziparchivereader.py"}
 
+
 def find_file_paths(dir_path: str) -> List[str]:
     all_files = os.listdir(dir_path)
     python_files = {fname for fname in all_files if ".py" == fname[-3:]}
@@ -22,12 +24,22 @@ def find_file_paths(dir_path: str) -> List[str]:
     return paths
 
 
+def extract_method_name(line: str) -> str:
+    if "(\"" in line:
+        start_token, end_token = "(\"", "\")"
+    elif "(\'" in line:
+        start_token, end_token = "(\'", "\')"
+    else:
+        raise RuntimeError(f"Unable to find appropriate method name within line:\n{line}")
+    start, end = line.find(start_token) + len(start_token), line.find(end_token)
+    return line[start:end]
+
+
 def parse_datapipe_file(filename):
     method_to_signature = {}
     with open(filename) as f:
         open_paren_count = 0
-        method_name = None
-        signature = ""
+        method_name, signature = "", ""
         skip = False
         for line in f.readlines():
             if line.count("\"\"\"") % 2 == 1:
@@ -35,14 +47,7 @@ def parse_datapipe_file(filename):
             if skip or "\"\"\"" in line:  # Skipping comment/example blocks
                 continue
             if "@functional_datapipe" in line:
-                if "(\"" in line:
-                    start = line.find("(\"") + len("(\"")
-                    end = line.find("\")")
-                    method_name = line[start:end]
-                elif "(\'" in line:
-                    start = line.find("(\'") + len("(\'")
-                    end = line.find("\')")
-                    method_name = line[start:end]
+                method_name = extract_method_name(line)
                 continue
             if method_name and ("def __init__(" in line or "def __new__(" in line):
                 open_paren_count += 1
@@ -54,27 +59,67 @@ def parse_datapipe_file(filename):
                 if open_paren_count == 0:
                     end = line.rfind(')')
                     signature += line[:end]
-                    method_to_signature[method_name] = signature
-                    method_name = None
-                    signature = ""
+                    method_to_signature[method_name] = process_signature(signature)
+                    method_name, signature = "", ""
                 elif open_paren_count < 0:
                     raise RuntimeError("open parenthesis count < 0. This shouldn't be possible.")
                 else:
                     signature += line.strip('\n').strip(' ')
-
     return method_to_signature
+
+
+# Split on comma unless it is within a bracket '[]'
+def split_outside_bracket(line: str, delimiter: str = ",") -> List[str]:
+    bracket_count = 0
+    curr_token = ""
+    res = []
+    for char in line:
+        if char == "[":
+            bracket_count += 1
+        elif char == "]":
+            bracket_count -= 1
+        elif char == delimiter and bracket_count == 0:
+            res.append(curr_token)
+            curr_token = ""
+            continue
+        curr_token += char
+    res.append(curr_token)
+    return res
+
+
+def process_signature(line: str) -> str:
+    tokens: List[str] = split_outside_bracket(line)
+    for i, token in enumerate(tokens):
+        tokens[i] = token.strip(' ')
+        if token == "cls":
+            tokens[i] = "self"
+        elif i > 0 and ("self" == tokens[i-1]) and (tokens[i][0] != "*"):
+            # Remove the datapipe after 'self' or 'cls' unless it has '*'
+            tokens[i] = ""
+        elif "Callable =" in token:  # Remove default argument if it is a function
+            head, default_arg = token.rsplit("=", 2)
+            tokens[i] = head.strip(' ') + "= ..."
+    tokens: List[str] = [t for t in tokens if t != ""]
+    line: str = ', '.join(tokens)
+    return line
+
 
 iter_datapipes_file_path = "datapipes/iter"
 file_paths = find_file_paths("datapipes/iter")
 methods_and_signatures = {}
 for file in file_paths:
     methods_and_signatures.update(parse_datapipe_file(file))
-for k, v in methods_and_signatures.items():
-    print()
-    print(f"method: {k}")
-    tokens = v.split(',')
-    tokens = [t.strip(' ') for t in tokens if t.strip(' ') != ""]
-    v = ', '.join(tokens)
-    print(v)
 
 
+method_definitions = []
+for method_name, signature in methods_and_signatures.items():
+    # TODO: We can add output type here, but they are all
+    #     1) mostly IterDataPipe
+    #     2) need to fix/double-check typing first within each DataPipe
+    # method_definitions.append(f"def {method_name}({signature}) -> {output_type}: ...")
+    method_definitions.append(f"def {method_name}({signature}): ...")
+
+fm = FileManager(install_dir='.', template_dir='.', dry_run=False)
+fm.write_with_template(filename="dataset.pyi",
+                       template_fn="dataset.pyi.in",
+                       env_callable=lambda: {'IterableDataPipeMethods': method_definitions})
