bbe877ff8f4c8f04330941a4e2f9e859664c6fcf
David Berard <dberard@fb.com>
2022-05-11 13:38:32 -0700

    implement caching ir visitor
    ... but running into issues with other implementations - turns out you
    probably can't just memoize that part away in llvm_codegen

--- torch/csrc/jit/tensorexpr/ir_visitor_caching.cpp
+++ torch/csrc/jit/tensorexpr/ir_visitor_caching.cpp
@@ -14,6 +14,34 @@ void IRVisitorCaching::save_cache(const std::shared_ptr<T>& ptr) {
   cache_.insert((void*) ptr.get());
 }
 
+#define DEFINE_VISIT(NamePtr)                    \
+  void IRVisitorCaching::visit(NamePtr v) {      \
+    if (is_cached(v)) {                          \
+      return;                                    \
+    }                                            \
+    save_cache(v);                               \
+    visit_impl(v);                               \
+  }                                              \
+                                                 \
+  void IRVisitorCaching::visit_impl(NamePtr v) { \
+    IRVisitor::visit(v);                         \
+  }
+#undef DEFINE_VISIT
+
+#define IMM_DEFINE_VISIT(Type, Name)                  \
+  void IRVisitorCaching::visit(Name##ImmPtr v) {      \
+    if (is_cached(v)) {                               \
+      return;                                         \
+    }                                                 \
+    save_cache(v);                                    \
+    visit_impl(v);                                    \
+  }                                                   \
+                                                      \
+  void IRVisitorCaching::visit_impl(Name##ImmPtr v) { \
+    IRVisitor::visit(v);                              \
+  }
+#undef IMM_DEFINE_VISIT
+
 } // namespace tensorexpr
 } // namespace jit
 } // namespace torch
--- torch/csrc/jit/tensorexpr/ir_visitor_caching.h
+++ torch/csrc/jit/tensorexpr/ir_visitor_caching.h
@@ -12,6 +12,18 @@ class IRVisitorCaching : public IRVisitor {
  private:
   std::unordered_set<void*> cache_;
 
+#define PRINT_VISIT(NamePtr)                    \
+  virtual void visit(NamePtr v) override final; \
+  virtual void visit_impl(NamePtr v);
+  FORALL_IR_VISITORS(PRINT_VISIT)
+#undef PRINT_VISIT
+
+#define IMM_PRINT_VISIT(Type, Name)                  \
+  virtual void visit(Name##ImmPtr v) override final; \
+  virtual void visit_impl(Name##ImmPtr v);
+  AT_FORALL_SCALAR_TYPES_AND3(Bool, Half, BFloat16, IMM_PRINT_VISIT)
+#undef IMM_PRINT_VISIT
+
   template <typename T>
   bool is_cached(const std::shared_ptr<T>& ptr);
   template <typename T>
