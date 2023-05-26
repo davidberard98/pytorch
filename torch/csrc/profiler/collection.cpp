dcfff453b2bdb9464d351b0a0a0273206fa8f337
David Berard <dberard@fb.com>
2023-05-26 11:37:42 -0700

    [WIP][profiler] Get fwd-bwd connections running again

--- torch/csrc/profiler/collection.cpp
+++ torch/csrc/profiler/collection.cpp
@@ -666,6 +666,96 @@ void mark_finished(std::shared_ptr<Result>& r) {
 
 static constexpr const char* indexKey = "Ev Idx";
 
+namespace {
+// Assumption: Total threads number will not exceed 2^16-1, and total ops will
+// not exceed 2^48 -1.
+static inline uint64_t getForwardThreadKey(uint64_t tid, uint64_t seqNr) {
+  return (((tid) << 48) | ((seqNr) & (((uint64_t)1 << 48) - 1)));
+}
+
+void generateForwardBackwardLinkV2(
+    const Result& profiler_result,
+    uint64_t& fwd_bwd_link_id,
+    libkineto::GenericTraceActivity& activity,
+    std::unordered_map<uint64_t, libkineto::GenericTraceActivity*>&
+        tidSeq2activity) {
+  const ExtraFields<EventType::TorchOp>& extra_fields =
+      c10::get<ExtraFields<EventType::TorchOp>>(profiler_result.extra_fields_);
+  if (extra_fields.forward_tid_ > 0) {
+    // act is backward op.
+    uint64_t key = getForwardThreadKey(
+        extra_fields.forward_tid_, extra_fields.sequence_number_);
+    auto iter = tidSeq2activity.find(key);
+    if (iter != tidSeq2activity.end()) {
+      libkineto::GenericTraceActivity* fwd = iter->second;
+      fwd->flow.start = true;
+      activity.flow.id = fwd->flow.id = fwd_bwd_link_id;
+      activity.flow.type = fwd->flow.type = libkineto::kLinkFwdBwd;
+      ++fwd_bwd_link_id;
+    }
+  } else if (profiler_result.start_tid_ != 0) {
+    uint64_t key = getForwardThreadKey(
+        profiler_result.start_tid_, extra_fields.sequence_number_);
+    // Assumption: Among all ops with same sequence number,
+    // the one with biggest start time is most likely launching backward op.
+    auto iter = tidSeq2activity.find(key);
+    if (iter == tidSeq2activity.end()) {
+      tidSeq2activity[key] = &activity;
+    } else {
+      // Now the sequence number is only incremented on creating a "Node"
+      // object for backward pass, by calling
+      // "at::sequence_number::get_and_increment()". Among all ops with same
+      // sequence number, the one with biggest startTime is the one launching
+      // backward op.
+      if (activity.startTime >= iter->second->startTime) {
+        tidSeq2activity[key] = &activity;
+      }
+    }
+  }
+}
+
+void generateForwardBackwardLinks(
+    std::unique_ptr<torch::profiler::impl::kineto::trace_t>& cpu_trace,
+    const std::vector<std::shared_ptr<Result>>& results) {
+#ifndef USE_KINETO
+}
+#else // USE_KINETO
+  TORCH_INTERNAL_ASSERT(cpu_trace->activities.size() == results.size());
+
+  // startThreadId_seqNum to pointer of activity.
+  // Low-16bits of startThreadId and low-48bits seqNum are concatenated into
+  // one uint64_t variable as key.
+
+  // From the time being, we need disable the forward/backward correlation
+  // feature to workaround the crash bug.
+  // TODO: by Mike Guo
+  // reenable the forward/backward correlation when kineto fix the following
+  // raw pointer
+  //    GenericTraceActivity.flow.linkedActivity
+
+  std::unordered_map<uint64_t, libkineto::GenericTraceActivity*>
+      tidSeq2activity;
+
+  uint64_t fwd_bwd_link_id = 1;
+
+  for (const auto idx : c10::irange(cpu_trace->activities.size())) {
+    auto& profiler_result = results[idx];
+    auto& activity = cpu_trace->activities[idx];
+
+    // add information about an associated forward op, if a sequence number
+    // is available (e.g. during training)
+
+    profiler_result->visit_if_base<ExtraFields<EventType::TorchOp>>(
+        [&](const auto&) {
+          generateForwardBackwardLinkV2(
+              *profiler_result, fwd_bwd_link_id, *activity, tidSeq2activity);
+        });
+  }
+}
+#endif
+
+} // namespace
+
 void passEventsToKineto(
     const std::vector<std::shared_ptr<Result>>& results,
     uint64_t start_time_us,
